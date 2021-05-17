998bbc6cd7ed68393aca74235a8dfa5fc791faa8
Richard <richard.sw268@gmail.com>
2021-05-16 23:36:59 -0500

    Links de nav

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Link from 'next/link';
 const Layout = (props) => {
   return (
     <>
       <h1>header</h1>
+      <nav>
+          <Link href="/">Inicio</Link>
+          <Link href="/nosotros">Nosotros</Link>
+      </nav>
       <main>{props.children}</main>
     </>
   );
---
+++ pages/nosotros.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+const Nosotros = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Nosotros xd</h1>
+      </Layout>
+    </div>
+  )
+}
+
+export default Nosotros
